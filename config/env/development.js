7be24e7ed6955ac82dc78755c0e51ce0f4666a21
JohnRajan95908 <johnrajan95908@gmail.com>
2023-12-04 11:52:10 -0800

    db authentification edited

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 3500,
     // db: "mongodb://127.0.0.1:27017/trekkwire",
-    db: "mongodb+srv://<username>:<password>@cluster0.ggeke.mongodb.net/trekkwire",
+    db: "mongodb+srv://pioneer:pioneer@cluster0.ggeke.mongodb.net/trekkwire",
     secret: "MYSECRETKEY"
 }
