3d5c2d404df47ad0a3e722edccb3effbadcef96b
JohnRajan95908 <johnrajan95908@gmail.com>
2023-12-04 11:12:13 -0800

    trekkwire database connection.

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 3500,
     // db: "mongodb://127.0.0.1:27017/trekkwire",
-    db: "mongodb+srv://takao:wIli6jmVh9Hc7rEw@cluster0.f4zpeiw.mongodb.net/trekkwire",
+    db: "mongodb+srv://<username>:<password>@cluster0.ggeke.mongodb.net/trekkwire",
     secret: "MYSECRETKEY"
 }
