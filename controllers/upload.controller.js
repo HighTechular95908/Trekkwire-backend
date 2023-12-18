38f11d789161a67cfd14561c49042d55df7ce4e3
luckyman816 <zhangyunpei816@gmail.com>
2023-12-17 23:48:47 -0800

    static uploads added

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -10,29 +10,38 @@ var mongoose = require("mongoose"),
 exports.uploadAvatar = catchAsync(async (req, res) => {
   let fileData = req.body;
   let filename = req.params.id; // filename = userId.
-  let mimeType = fileData.split(";")[0].split(":")[1];
-  let fileType = mimeType.split("/")[1];
-  fileType = "jpeg";
-  let filePath = path.join(
-    __dirname,
-    `..${PATH_AVATAR_UPLOAD}`,
-    `${filename}.${fileType}`
-  );
-  var base64Data = fileData.split(";base64,").pop();
-  fs.writeFile(
-    filePath,
-    base64Data,
-    { encoding: "base64" },
-    async function (err) {
-      if (err) {
-        console.log(err);
-        handleError(err, res);
-      } else {
-        await User.findByIdAndUpdate(filename, {
-          avatar: `${PATH_AVATAR_UPLOAD}${filename}.${fileType}`,
-        });
-        return res.status(200).send({});
-      }
-    }
-  );
+
+  // let mimeType = fileData.split(";")[0].split(":")[1];
+  // let fileType = mimeType.split("/")[1];
+  // fileType = "jpeg";
+  // let filePath = path.join(
+  //   __dirname,
+  //   `..${PATH_AVATAR_UPLOAD}`,
+  //   `${filename}.${fileType}`
+  // );
+  // var base64Data = fileData.split(";base64,").pop();
+  // fs.writeFile(
+  //   filePath,
+  //   base64Data,
+  //   { encoding: "base64" },
+  //   async function (err) {
+  //     if (err) {
+  //       console.log(err);
+  //       handleError(err, res);
+  //     } else {
+  //       await User.findByIdAndUpdate(filename, {
+  //         avatar: `${PATH_AVATAR_UPLOAD}${filename}.${fileType}`,
+  //       });
+  //       return res.status(200).send({});
+  //     }
+  //   }
+  // );
+  try{
+    // await User.
+  }
+  catch(err){
+    handleError(err, res);
+  }
+
+
 });
